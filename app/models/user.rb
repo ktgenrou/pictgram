088dff1f502c897fa227e4da25652596f36a37ca
EC2 Default User <ec2-user@ip-172-31-6-104.ap-northeast-1.compute.internal>
2020-06-30 10:58:33 +0000

    rails04課題

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,15 @@
 class User < ApplicationRecord
-  validates :name, presence: true 
-  validates :email, presence: true , uniqueness: true
+  validates :name,
+    presence: true ,
+    length: { maximum: 15 }
+  validates :email, 
+    presence: true , 
+    uniqueness: true,
+    format: { with: /\A\S+@\S+\.\S+\z/}
+  validates :password, 
+    presence: true, 
+    length: { in: 8..32 }, 
+    format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{8,32}+\z/i }
   
   has_secure_password
 end
